bab98ed50e6b47e9e66fb738a44c461b80a5e166
Javi <clony90@gmail.com>
2018-05-30 13:48:23 +0100

    fix folder structure

--- test.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-
-account_api = Blueprint('account_api', __name__)
-
-@account_api.route('/', methods=['GET'])
-def test():
-    return jsonify({'info' : 'API REST - VELANDO CRUZ, JAVIER'})
-
